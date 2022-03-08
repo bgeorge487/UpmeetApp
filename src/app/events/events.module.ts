a68a1084ec3a1c16567883c6b97519c322024fa3
Brian G <brigeo987@gmail.com>
2022-03-07 19:15:17 -0500

    angular project initial commit

---
+++ src/app/events/events.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventDetailsComponent } from './event-details/event-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    EventDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EventsModule { }
